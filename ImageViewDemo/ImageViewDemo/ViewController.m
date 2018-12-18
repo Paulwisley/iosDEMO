33fe4e3a5f501e71af07b1a2db1ecde3dc0b7ad2
“Paulwisley” <1134556973@qq.com>
2018-12-18 23:46:21 +0800

    change arr to strong

--- ImageViewDemo/ImageViewDemo/ViewController.m
+++ ImageViewDemo/ImageViewDemo/ViewController.m
@@ -20,7 +20,8 @@
 @property(nonatomic,weak) IBOutlet UIButton *leftBtn;
 @property(nonatomic,weak)IBOutlet UIButton *rightBtn;
 @property(nonatomic,assign) int index;
-@property(nonatomic,assign) NSArray *arr;
+@property(nonatomic,strong) NSArray *arr;
+//@property(nonatomic,assign) NSDictionary *dict;
 -(IBAction) btnLeft;
 -(IBAction) btnRight;
 @end
@@ -34,17 +35,6 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
--(NSArray *)Array
-{
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"Imagetext" ofType:@"plist"];
-    if(_arr == nil)
-    {
-        _arr = [NSArray arrayWithContentsOfFile:path];
-        NSLog(@"%@",_arr);
-    }
-    return _arr;
-}
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
@@ -54,42 +44,54 @@
 {
     self.index--;
     [self changeIndex];
-    self.leftBtn.enabled = self.index != 0;
 }
 
 -(IBAction)btnRight
 {
     self.index++;
+//    NSLog(@"%d",self.index);
     [self changeIndex];
-    self.rightBtn.enabled = self.index != 5;
+}
+
+-(NSArray *)arr
+{
+    if(_arr == nil){
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"Imagetext" ofType:@"plist"];
+        _arr = [NSArray arrayWithContentsOfFile:path];
+        //    NSLog(@"%@",_arr);
+    }
+    return _arr;
 }
 
 -(void)changeIndex
 {
-    NSArray *imageArr = [self Array];
+    NSDictionary *dict = self.arr[self.index];
     //NSLog(@"%@",[imageArr objectAtIndex:self.index]);
     switch (self.index) {
         case 0:
-            [self setImage:[imageArr objectAtIndex:self.index][@"icon"] setIndex:self.index setText:[imageArr objectAtIndex:self.index][@"text"]];
+            [self setImage:dict[@"icon"] setIndex:self.index setText:dict[@"text"]];
             break;
         case 1:
-         [self setImage:[imageArr objectAtIndex:self.index][@"icon"] setIndex:self.index setText:[imageArr objectAtIndex:self.index][@"text"]];
+            [self setImage:dict[@"icon"] setIndex:self.index setText:dict[@"text"]];
             break;
         case 2:
-        [self setImage:[imageArr objectAtIndex:self.index][@"icon"] setIndex:self.index setText:[imageArr objectAtIndex:self.index][@"text"]];
+            [self setImage:dict[@"icon"] setIndex:self.index setText:dict[@"text"]];
             break;
         case 3:
-        [self setImage:[imageArr objectAtIndex:self.index][@"icon"] setIndex:self.index setText:[imageArr objectAtIndex:self.index][@"text"]];
+            [self setImage:dict[@"icon"] setIndex:self.index setText:dict[@"text"]];
             break;
         default:
             break;
     }
+    self.leftBtn.enabled = self.index != 0;
+    self.rightBtn.enabled = self.index != self.arr.count - 1;
+    
 }
 
 -(void) setImage:(NSString *)imagename setIndex:(int) index setText:(NSString *) text
 {
     self.imageView.image = [UIImage imageNamed:imagename];
-    self.labelTitle.text = [NSString stringWithFormat:@"%d/%lu",index,[self Array].count-0];
+    self.labelTitle.text = [NSString stringWithFormat:@"%d/%lu",self.index + 1, self.arr.count];
     self.labelFoot.text = text;
 }
 
