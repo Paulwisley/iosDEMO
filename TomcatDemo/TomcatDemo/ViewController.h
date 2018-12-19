6efe388829689b928c0ebc7d5a8581499f4044ca
Paulwisley <1134556973@qq.com>
2018-12-19 17:06:32 +0800

    tomcat init

---
+++ TomcatDemo/TomcatDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TomcatDemo
+//
+//  Created by Paul on 2018/12/19.
+//  Copyright © 2018 Dingzhijian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TomcatDemo/TomcatDemo/ViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  TomcatDemo
+//
+//  Created by Paul on 2018/12/19.
+//  Copyright © 2018 Dingzhijian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property(nonatomic,weak) IBOutlet UIImageView *imag;
+@property(nonatomic,weak) IBOutlet UIButton *btnDrink;
+
+-(IBAction)drinkMilk;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)drinkMilk
+{
+    NSMutableArray *images = [NSMutableArray array];
+    for(int i = 0 ; i < 81; i++)
+    {
+        NSString *imagename = [NSString stringWithFormat:@"drink_%02d",i];
+        UIImage *img = [UIImage imageNamed:imagename];
+        [images addObject:img];
+    }
+    // [UIImage animatedImageNamed:<#(nonnull NSString *)#> duration:<#(NSTimeInterval)#>]
+    //[UIView beginAnimations: context:]
+}
+
+-(void)changeImage
+{
+    
+}
+
+@end
